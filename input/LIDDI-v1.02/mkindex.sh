4d984f8423f06cb3290f1dc97d668041e566fe42
Tobias Kuhn <kuhntobias@gmail.com>
2018-03-30 15:16:59 +0200

    Add license info for LIDDI

--- input/LIDDI-v1.02/mkindex.sh
+++ input/LIDDI-v1.02/mkindex.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 np mkindex \
-  -o index.LIDDIv1.02.trig.gz \
+  -o files/index.LIDDIv1.02.trig.gz \
   -t "Linked Drug-Drug Interactions (LIDDI) dataset v1.02" \
   -d "This dataset combines data on drug-drug interactions from different sources, and formats them as nanopublications. For details see: Banda et al. Provenance-Centered Dataset of Drug-Drug Interactions. In Proceedings of ISWC 2015, Springer." \
   -a http://liddi.stanford.edu \
   -c 0000-0001-8499-824X \
-  trusty.LIDDIv1.02.trig.gz
+  -l https://creativecommons.org/licenses/by-nc/4.0/ \
+  files/trusty.LIDDIv1.02.trig.gz
 

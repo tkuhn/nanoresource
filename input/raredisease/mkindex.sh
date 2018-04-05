9bb8e3b47e6264cc6cc0b3f0ced97b2737ffe7fe
Tobias Kuhn <kuhntobias@gmail.com>
2018-04-05 10:52:14 +0200

    Make index for Freddie's data

---
+++ input/raredisease/mkindex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+np mkindex \
+  -o index.association.trig \
+  -t "Monogenic rare diseases: gene associations with specific literature references" \
+  -c 0000-0001-6048-1457 \
+  -c 0000-0002-7770-620X \
+  -c 0000-0001-7542-0286 \
+  association.trig
+

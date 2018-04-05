c49f2bbfb7c829063717533ff2c8791e10f93070
Tobias Kuhn <kuhntobias@gmail.com>
2018-04-05 12:15:25 +0200

    Make whole data dump

--- npdump/get-dump.sh
+++ npdump/get-dump.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for C in {1..10798}; do
+for C in {1..10803}; do
   N=$(printf "%05d" $C)
   mongofiles --db nanopub-server --prefix packages_gzipped -l dump/$N.trig.gz get $C
 done
@@ -13,3 +13,17 @@ ls dump \
   | grep .trig.gz \
   | awk '{ print "cat dump/"$1" >> combined-dump.trig.gz" }' \
   | bash
+
+# Get nanopubs of last page:
+
+rm -f last-page.trig
+
+wget -O - http://localhost:8080/nanopubs.txt?page=10804 \
+  | tr -d '\015' \
+  | grep -o '.\{45\}$' \
+  | awk '{ print "wget -O - http://localhost:8080/"$1".trig >> last-page.trig" }' \
+  | bash
+
+gzip last-page.trig
+
+cat last-page.trig.gz >> combined-dump.trig.gz

aaa69ab56d11b5ca59d26562593ec3548425202a
Tobias Kuhn <kuhntobias@gmail.com>
2018-04-05 12:17:24 +0200

    Update namespace script

--- scripts/get-namespaces.sh
+++ scripts/get-namespaces.sh
@@ -5,11 +5,18 @@ npop namespaces --subj \
   -a namespaces/all-a-subj.txt.gz \
   -p namespaces/all-p-subj.txt.gz \
   -i namespaces/all-i-subj.txt.gz \
-  npdump/*
+  npdump/combined-dump.trig.gz
+
+npop namespaces --pred \
+  -h namespaces/all-h-pred.txt.gz \
+  -a namespaces/all-a-pred.txt.gz \
+  -p namespaces/all-p-pred.txt.gz \
+  -i namespaces/all-i-pred.txt.gz \
+  npdump/combined-dump.trig.gz
 
 npop namespaces --obj \
   -h namespaces/all-h-obj.txt.gz \
   -a namespaces/all-a-obj.txt.gz \
   -p namespaces/all-p-obj.txt.gz \
   -i namespaces/all-i-obj.txt.gz \
-  npdump/*
+  npdump/combined-dump.trig.gz

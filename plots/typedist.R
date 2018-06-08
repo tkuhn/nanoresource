687b18cb7f0f1a219c57041c29427611adf3112b
Tobias Kuhn <kuhntobias@gmail.com>
2018-06-08 14:59:19 +0200

    Update plot

--- plots/typedist.R
+++ plots/typedist.R
@@ -2,7 +2,7 @@
 
 typerankdata = read.csv("../types/type-ranks.csv", header=TRUE)
 
-pdf("typedist.pdf", width=5, height=5)
+pdf("typedist.pdf", width=5, height=4)
+par(mar=c(4,4,0.5,0.5))
+plot( count ~ rank, data = typerankdata, log = "xy" )
 
-plot( count ~ rank, data = typerankdata, log = "xy", main = "Frequency distribution of types" )
-c
